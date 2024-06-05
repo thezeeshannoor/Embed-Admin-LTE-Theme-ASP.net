3b54326a3415bd4100a80ae4bfbda7bfc7425ff8
unknown <zeeshannoorofficial@gmail.com>
2024-06-05 18:06:18 +0500

    Embed Admin Lte Theme

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Admin_Pannel_Embeded.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
